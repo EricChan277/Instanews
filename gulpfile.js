d2d4dbeebbdf35a2c74322d17ade520109d07618
Eric Chan <ericchan277@gmail.com>
2018-01-24 16:11:46 -0800

    Added GULP
    Added GULP files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,44 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),           //Always first in load order
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync'),
+  eslint = require('gulp-eslint');
 
-gulp.task('default', defaultTask);
 
-function defaultTask(done) {
-  // place code for your default task here
-  done();
-}
+
+
+gulp.task('watch', function() {
+  gulp.watch('./js/*.js', gulp.series('script')),
+  gulp.watch('./style.css', gulp.series('css'))
+});
+
+gulp.task('css', function() {
+  return gulp.src('./style.css')
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./js/*.js')
+  .pipe(eslint())   //calls eslint to run
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError())
+})
+
+gulp.task('script', gulp.series('lint') ,function() {
+  return gulp.src('./js/*.js')
+    .pipe(uglify())   //calls uglify to run
+    .pipe(rename({extname: '.min.js'})) //renames extension to ".min.js" 
+    .pipe(gulp.dest('./build/js')); //places in ./build/js folder
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init( {
+    server: {
+      baseDir: "./"
+    }
+  })
+})
+
+gulp.watch("./build/js/*.js").on('change', browserSync.reload);
+gulp.watch("./style.css").on('change', browserSync.reload);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync')); //runs script in parallel
--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-// Built by LucyBot. www.lucybot.com
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "a0f2fb5adadf4660ac0eb18fbb12876e"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+// // Built by LucyBot. www.lucybot.com
+// var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+// url += '?' + $.param({
+//   'api-key': "a0f2fb5adadf4660ac0eb18fbb12876e"
+// });
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function(result) {
+//   console.log(result);
+// }).fail(function(err) {
+//   throw err;
+// });
+alert("HELlo");
