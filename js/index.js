17f905bff3ff8b5387c00544308c305df9f10d83
Eric Chan <ericchan277@gmail.com>
2018-01-27 15:00:27 -0800

    Switched footer and Content from css to SCSS

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + $.param({
-  'api-key': 'a0f2fb5adadf4660ac0eb18fbb12876e'
+  'api-key': "a0f2fb5adadf4660ac0eb18fbb12876e"
 });
 $.ajax({
   url: url,
